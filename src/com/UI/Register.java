becfd91371a5d419a8231dbd2c7e6566b893a5d0
杨岩 <2561952525@qq.com>
2021-07-14 09:50:13 +0800

    1

--- src/com/UI/Register.java
+++ src/com/UI/Register.java
@@ -1,5 +1,8 @@
 package com.UI;
 
+import com.DAO.UserDAO;
+import com.Interface.LoginAndRegister;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -7,16 +10,18 @@ import java.awt.event.ActionListener;
 
 public class Register extends JFrame {
     public Register() {
-        JFrame frame = new JFrame("添加用户");
+        UserDAO ud=new UserDAO();
+        JFrame frame = new JFrame("用户注册");
         JPanel panel = new JPanel();
         frame.setSize(650, 720);
         frame.setLocationRelativeTo(null);                     //在屏幕中居中显示
         frame.add(panel);                                      // 添加面板
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // 设置X号后关闭
         panel.setLayout(null);
-        panel.setBackground(Color.getHSBColor(201,175,138));
-        JLabel label1=new JLabel("添加用户");
-        JLabel userLabel = new JLabel("用户名:");           // 创建UserJLabel
+        JTextField idtext=new JTextField();         //获取id
+        JLabel idlabel = new JLabel("账号");
+        JLabel label1=new JLabel("用户注册");
+        JLabel userLabel = new JLabel("昵称:");           // 创建UserJLabel
         JTextField userText = new JTextField();           // 获取登录名
         JLabel passLabel = new JLabel("密 码:");       // 创建PassJLabel
         JPasswordField passText = new JPasswordField(20); //密码框隐藏
@@ -30,52 +35,51 @@ public class Register extends JFrame {
         JButton button2=new JButton("登录");
         JButton button3=new JButton("注册");
 
-        label1.setBounds(200,50,450,70);
+        label1.setBounds(250,10,450,70);
         label1.setFont(new Font("宋体",Font.BOLD,40));
-
-        userLabel.setBounds(50, 150, 100, 65);
+        idlabel.setBounds(50, 150+11-70, 100, 54);
+        idlabel.setFont(new Font("宋体",Font.BOLD,20));
+        idtext.setBounds(150, 150+11-70, 450, 54);
+        idtext.setFont(new Font("宋体",Font.BOLD,20));
+        userLabel.setBounds(50, 150+11, 100, 54);
         userLabel.setFont(new Font("宋体",Font.BOLD,20));
 
-        userText.setBounds(150, 150, 450, 65);
+        userText.setBounds(150, 150+11, 450, 54);
         userText.setFont(new Font("宋体",Font.BOLD,20));
         userText.setBorder(BorderFactory.createLineBorder(Color.white, 2, true));
 
-        passLabel.setBounds(50,220 , 100, 65);
+        passLabel.setBounds(50,220+11 , 100, 54);
         passLabel.setFont(new Font("宋体",Font.BOLD,20));
 
-        passText.setBounds(150, 220, 450, 65);
+        passText.setBounds(150, 220+11, 450, 54);
         passText.setFont(new Font("宋体",Font.BOLD,20));
         passText.setBorder(BorderFactory.createLineBorder(Color.white, 2, true));
 
-        passLabel1.setBounds(50,290,100,65);
+        passLabel1.setBounds(50,290+11,100,54);
         passLabel1.setFont(new Font("宋体",Font.BOLD,14));
 
-        passText1.setBounds(150,290,450,65);
+        passText1.setBounds(150,290+11,450,54);
         passText1.setFont(new Font("宋体",Font.BOLD,20));
 
-        question.setBounds(50,360,100,65);
+        question.setBounds(50,360+11,100,54);
         question.setFont(new Font("宋体",Font.BOLD,20));
 
-        questionText.setBounds(150,360,450,65);
+        questionText.setBounds(150,360+11,450,54);
         questionText.setFont(new Font("宋体",Font.BOLD,20));
 
-        answer.setBounds(50,430,100,65);
+        answer.setBounds(50,430+11,100,54);
         answer.setFont(new Font("宋体",Font.BOLD,20));
 
-        answerText.setBounds(150,430,450,65);
+        answerText.setBounds(150,430+11,450,54);
         answerText.setFont(new Font("宋体",Font.BOLD,20));
 
         button1.setBounds(0,624,325,50);
         button1.setFont(new Font("宋体",Font.BOLD,30));
-        button1.setBackground(Color.getHSBColor(201,175,138));
-        button1.setBorder(BorderFactory.createLineBorder(Color.getHSBColor(201,175,138), 0, true));
         button1.setEnabled(false);
 
 
         button2.setBounds(325,624,325,50);
         button2.setFont(new Font("宋体",Font.BOLD,30));
-        button2.setBackground(Color.getHSBColor(230,220,205));
-        button2.setBorder(BorderFactory.createLineBorder(Color.RED, 0, true));
         button2.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -86,7 +90,7 @@ public class Register extends JFrame {
             }
         });
 
-        button3.setBounds(100,500,450,65);
+        button3.setBounds(325-125,500,250,65);
         button3.setBackground(Color.RED);
         button3.setFont(new Font("宋体",Font.BOLD,30));
         button3.setBorder(BorderFactory.createLineBorder(Color.RED, 0, true));
@@ -95,13 +99,40 @@ public class Register extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 if(e.getSource()==button3){
-                        JOptionPane.showMessageDialog(
+                    int i=ud.register(idtext.getText(),userText.getText(), String.valueOf(passText.getPassword()), questionText.getText(), answerText.getText(),0);
+                    switch (i)
+                    {
+                        case 1:JOptionPane.showMessageDialog(
                                 frame,
                                 "注册成功",
                                 "消息标题",
                                 JOptionPane.INFORMATION_MESSAGE
-                        );
+                        );break;
 
+                        case 0:JOptionPane.showMessageDialog(
+                                frame,
+                                "注册失败，用户ID不符合要求，请输入3-16位由字母数字组成",
+                                "消息标题",
+                                JOptionPane.INFORMATION_MESSAGE
+                        );break;
+
+                        case -1:
+                            JOptionPane.showMessageDialog(
+                                    frame,
+                                    "用户名重复",
+                                    "消息标题",
+                                    JOptionPane.INFORMATION_MESSAGE
+                            );break;
+
+                        case -2:JOptionPane.showMessageDialog(
+                                frame,
+                                "注册失败，密码6-20位字母数字组成",
+                                "消息标题",
+                                JOptionPane.INFORMATION_MESSAGE
+                        );break;
+
+
+                    }
                 }
             }
         });
@@ -119,6 +150,8 @@ public class Register extends JFrame {
         panel.add(answerText);
         panel.add(button1);
         panel.add(button2);
+        panel.add(idlabel);
+        panel.add(idtext);
         frame.setVisible(true);
 
     }
